d11a63679369a9570289ce15f7b8426125062a09
Sushree Sonali Panda <70882049+risingphoenixx@users.noreply.github.com>
2021-09-07 12:18:33 +0530

    Update querry1.sql

--- querry1.sql
+++ querry1.sql
@@ -5,9 +5,9 @@ create table users(Name varchar(100),User_no int,User_email varchar(60));
 create table codekata(Student_Roll_No int,Problem_Solved int);
 create table attendance(Name Varchar(100),Student_roll_no int,Present_Days varchar(7),Absent_Days varchar (9));
 create table topics(Subject_name varchar(40), Topic_name varchar(40),total_problems int);
-create table tasks(Task_name varchar(40),Task_number int,Task_Complete int);
+create table tasks(Student_roll_no int,Subject_name varchar(40),Task_name int,Task_Complete varchar(3);
 create table company_drives(Student_roll_no int,Company_name varchar(50));
-create table mentors(Mentora_ID int,mentor_name varchar(70),Total_Task int,Students_Assigned int,Student_Name Varchar(60));
+create table mentors(Mentora_ID int,mentor_name varchar(70),Students_Assigned int,Student_Name Varchar(60));
 create table students_activated_courses(Student_roll_no int,courses_taken varchar(45),completed_course int);
 create table courses(course_name varchar(70));
 ---Insert Rows in each table----
@@ -20,5 +20,11 @@ INSERT INTO attendance(Name,Student_Roll_no,Present_Days,Absent_Days)
 VALUES('Rain Martin','87348','22','8'),('Stephen Hardson','85349','21','9'),('Kochik Stephard','43628','28','2'),('Gowchick S. Kandeson','37893','24','6'),
 ('H.landersiv','39203','25','5');
 INSERT INTO topics(Subject_name,Topic_name,total_problems)
-VALUES('#C','Pointer','120'),('Java','Array','90'),('Python','Slicing','120'),('React',),()
+VALUES('#C','Pointer','120'),('Java','Array','90'),('Python','Slicing','120'),('React','Components','220'),('MySql','Insert','20');
+INSERT INTO tasks(Student_roll_no,Subject_name,Task_name,Task_Complete)
+VALUES('43839','#C','Pointer','Yes'),('78473','Java','Array','No'),('84394','Python','Slicing','No'),('84744','React','Components','Yes'),('89534','MySql','Insert','Yes');
+INSERT INTO company_drives(Student_roll_no,Company_name);
+VALUES('75844','CHUBB'),('87493','Walmart'),('83473','Microsoft'),('54437','EY'),('65487','Morgan Stanley');
+INSERT INTO  mentors(Mentora_ID,mentor_name,Students_Assigned,Student_Name);
+       
 
