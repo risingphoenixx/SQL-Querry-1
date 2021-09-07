ba9442ecc1e8fe2d29154d38fb3161152798585d
Sushree Sonali Panda <70882049+risingphoenixx@users.noreply.github.com>
2021-09-07 11:07:10 +0530

    Update querry1.sql

--- querry1.sql
+++ querry1.sql
@@ -1,3 +1,4 @@
+----Creating table with some columns----
 Create database db;
 use db;
 create table users(Name varchar(100),User_no int,User_email varchar(60));
@@ -9,3 +10,8 @@ create table company_drives(Student_roll_no int,Company_name varchar(50));
 create table mentors(Mentora_ID int,mentor_name varchar(70),Total_Task int,Students_Assigned int,Student_Name Varchar(60));
 create table students_activated_courses(Student_roll_no int,courses_taken varchar(45),completed_course int);
 create table courses(course_name varchar(70));
+---Insert Rows in each table----
+INSERT INTO users(name,User_no,User_email)
+VALUES('Richard','23078','richard12@gmail.com'),('S.T Hanson','34783','shanson@gmail.com'),('Chitav','48383','chitav848@gmail.com'),('Simon','93039','simon39@gmail.com'),
+('Lander Krisza','43950','kriszalander@gmail.com');
+
